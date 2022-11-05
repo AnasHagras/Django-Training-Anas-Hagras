52a29076e98dae860928ef84f71831f77a0e6851
= <anas.hagras1999@gmail.com>
2022-11-05 05:27:56 +0200

    fix task8 review issues

--- artists/models.py
+++ artists/models.py
@@ -3,7 +3,7 @@ from users.models import User
 class Artist(models.Model):
     stageName = models.CharField(max_length=50 , unique = True, null = False , blank = False)
     socialLink = models.URLField(max_length=100)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null = True)
     def __str__ (self):
         return self.stageName
     
